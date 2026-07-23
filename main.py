d664e2859c76165c0db1de1de2be2e858752b154
Sofya Nekipelova <MadeThroughLove@yandex.ru>
2026-07-24 02:27:47 +0300

    Add dotenv author config

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello from repository!')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def print_author():
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+
+print_author()
